75a0f2452d175e1e9ffe6846a98533950c8bfc49
S. M. Abrar Jahin <abrarjahin@live.com>
2018-01-11 17:16:01 +0600

    Initial Commit

---
+++ InventoryManagement/InventoryManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class InventoryManagementDbContext : DbContext
+{
+    public InventoryManagementDbContext(DbContextOptions<InventoryManagementDbContext> options)
+    : base(options)
+    {
+        //Migration - create Tables
+        try
+        {
+            this.Database.EnsureDeleted();
+            // Migrate the database
+            this.Database.EnsureCreated();
+            this.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+        }
+    }
+}
---
+++ InventoryManagement/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryManagement
+{
+    public class Startup
+    {
+        private ILogger _logger;
+        private IHostingEnvironment env;
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory, IHostingEnvironment env)
+        {
+            _logger = loggerFactory.CreateLogger<Startup>();
+            //Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddMvc();
+            //Configure Connection
+            services.AddDbContext<InventoryManagementDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+            // Add EntityFramework Service.
+            services.AddEntityFrameworkSqlite().AddDbContext<InventoryManagementDbContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
